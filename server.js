87cd5226dbb1476c7dc00488a855fc1589e3b56b
anthonychung14 <anthonychung14@gmail.com>
2015-12-30 15:00:46 -0800

    I wish there was a better way

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ var process = require('./process.env');
 
 app.listen(process.PORT || 5000);
 
-console.log('Server now listening on port ' + port);
+console.log('Server now listening on port ' + process.PORT);
